25633aa73719e343e8769e80941b6199bc83b16e
Nicholas Davidson <ndavidson.sf@me.com>
2016-09-04 18:33:04 -0700

    Completes initial migrations/Ready to migrate DB

--- db/migrate/20160904181508_create_addresses.rb
+++ db/migrate/20160904181508_create_addresses.rb
@@ -1,4 +1,14 @@
 class CreateAddresses < ActiveRecord::Migration
   def change
+    create_table :addresses do |t|
+      t.string :label, null: false, length: 64
+      t.string :address_1, null: false, length: 128
+      t.string :address_2, null: false, length: 128
+      t.string :city, null: false, length: 64
+      t.string :state, null: false, length: 64
+      t.string :zip_code, null: false, length: 64
+
+      t.timestamps null: false
+    end
   end
 end

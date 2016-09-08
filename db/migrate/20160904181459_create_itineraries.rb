d641a34ebb9620a69064391d36da353901392426
Nicholas Davidson <ndavidson.sf@me.com>
2016-09-08 11:04:57 -0700

    db migrations

--- db/migrate/20160904181459_create_itineraries.rb
+++ db/migrate/20160904181459_create_itineraries.rb
@@ -2,6 +2,7 @@ class CreateItineraries < ActiveRecord::Migration
   def change
     create_table :itineraries do |t|
       t.string :label, null: false, length: 64
+      t.date :date, null: false
       t.string :airline, null: false
       t.string :flight_number, null: false
       t.string :origin, null: false
